1938406251573bb6932fe059c7aec7d3b8adf1ce
edbaymax <ednd78@gmail.com>
2019-12-03 09:55:44 +0000

    another conf

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,19 +1,14 @@
 const Mongoose = require('mongoose');
 
-Mongoose.connect(
-  'mongodb://localhost/QR-FARE' ||
-    process.env.MONGO_URL ||
-    process.env.MONGO_URI,
-  {
-    useNewUrlParser: true,
-    connectTimeoutMS: 24000,
-    socketTimeoutMS: 24000,
-  },
-);
+Mongoose.connect(process.env.MONGO_URI || process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  connectTimeoutMS: 24000,
+  socketTimeoutMS: 24000,
+});
 
 Mongoose.connection.on('open', () => console.log('Opening connection...'));
 Mongoose.connection.on('connected', () => console.log('Connected'));
 Mongoose.connection.on('error', () => console.error);
-Mongoose.connection.on('disconnected', () => console.log('Disconnected'));
+Mongoose.connection.on('disconnected', () => console.error);
 
 module.exports = Mongoose;
